f6c9325e33534633028227059948d6a3ab1c1fc9
sholabioku <bilushonline@gmail.com>
2019-03-05 22:46:00 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -41,9 +41,19 @@ app.listen(8080, () => {
 
 app.get('/todos/:id', (req, res) => {
     let id = req.params.id;
+    //Validate id using isValid
+    //404-send back empty send
+
     if (!ObjectID.isValid(id)) {
         return res.status(404).send()
     }
+
+    //findById
+        //Success
+            //if todo-send it back
+            //if no todo-send back 404 with empty body
+        //Error
+            //400-and send empty body back
     Todo.findById(id)
         .then((todo) => {
             if (!todo) {
