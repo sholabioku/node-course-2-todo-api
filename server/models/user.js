3b86a91f4eebe4023dd6f265c7021f9704a0a653
sholabioku <bilushonline@gmail.com>
2019-03-14 12:23:11 +0100

    Add POST /users/login routes

--- server/models/user.js
+++ server/models/user.js
@@ -83,6 +83,30 @@ UserSchema.pre('save', function(next) {
     }
 });
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+
+        } else {
+          reject();
+
+        };
+
+      });
+    });
+  });
+
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
