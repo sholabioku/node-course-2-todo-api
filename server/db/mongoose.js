595d12eed34b91c31f41f8d384f22fdfcf799295
sholabioku <bilushonline@gmail.com>
2019-03-14 11:15:30 +0100

    Add GET /users/me and POST /users test routes and findOneAndUpdate modify to false

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,12 @@ const mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI,  {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
-mongoose.set('useCreateIndex', true);
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useFindAndModify', false);
 
 
 module.exports = {mongoose}
