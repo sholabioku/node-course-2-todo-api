659a753ba62c47a135907c1133f10b3c78f68449
sholabioku <bilushonline@gmail.com>
2019-03-24 12:32:51 +0100

    Change message for bad request on POST /users/login

--- server/server.js
+++ server/server.js
@@ -185,7 +185,7 @@ app.post('/users/login', (req, res) => {
     }).catch((e) => {
         res.status(400).send({
             error: e,
-            message: 'Auth failed'
+            message: 'User does not exist'
         });
     });
 });
